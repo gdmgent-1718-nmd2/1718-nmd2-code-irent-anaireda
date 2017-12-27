9ee9b2e93731b0712bddecc1b3bd52e004761cd0
Anaïs Redant <anaireda@student.arteveldehs.be>
2017-12-27 16:26:01 +0100

    recensies af behalve de sterren

---
+++ assets/js/rating.js
@@ -0,0 +1,8 @@
+$('.star').click(function(e){
+  var length = $('.review-container .star').length;
+  var selected = $('.review-container .star').index($(this));
+  
+  $( ".review-container .star" ).each(function( index ) {
+  if(index <= selected){
+    
+  $(this).addClass("active");
